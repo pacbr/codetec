ec942ca0955189e7752adaeba14e16ddabe3f1a3
pakopv <pakopv@3d456fcf-c8b1-d877-8f84-fd8412655159>
2011-07-06 17:37:29 +0000

    Mejorado parpadeo de imagen gris para que al pinchar varias veces sobre la imagen no entre en un bucle infinito.

--- trunk/src/clases/Grafico.java
+++ trunk/src/clases/Grafico.java
@@ -9,7 +9,6 @@ public class Grafico extends JFrame{
 	public Grafico(JLabel et, String nombre){
 		super(nombre);
 		
-		//Agrego etiqueta que contiene la imagen al frame
 		getContentPane().add(et);
 		
 		this.setSize(500, 500);
@@ -23,59 +22,54 @@ public class Grafico extends JFrame{
     int i;
     int cont;
     Timer timer;
+    
 	public Grafico(JLabel et, String nombre, JLabel jletiqueta){
 		super(nombre);
 		et1 = et;
 		et2 = jletiqueta;
-		//Agrego etiqueta que contiene la imagen al frame
-//		getContentPane().add(et);
+		timer = new Timer (300, new ActionListener ());
+
 		this.setSize(700, 700);
 		this.setLocationRelativeTo(null);
 		esqueGris = new CardLayout();
 		setLayout(esqueGris);
 		getContentPane().add("jlabel1",et1);
 		getContentPane().add("jlabel2",et2);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		show();
 		
 		new PointSelector(this);
 	}
 	
 
-    
-    public void cambiaImagen(){
-    	timer = new Timer (300, new ActionListener ()
-		
-		{
-    		
-    		public void actionPerformed(ActionEvent e){
-		    	if (i==1){
-					esqueGris.show(g.getContentPane(),"jlabel2");
-					i=2;
-				}else{
-					esqueGris.show(g.getContentPane(),"jlabel1");
-					i=1;
-				}
-		    	cont++;
-		    	System.out.println(cont);
-		    	if(cont==6){
-		    		timer.stop();
-				}
+	class ActionListener implements java.awt.event.ActionListener{		
+		public void actionPerformed(ActionEvent e){
+	    	if (i==1){
+				esqueGris.show(g.getContentPane(),"jlabel2");
+				i=2;
+			}else{
+				esqueGris.show(g.getContentPane(),"jlabel1");
+				i=1;
 			}
-		});
-		
-		timer.start();
+	    	cont++;
+	    	System.out.println(cont);
+	    	if(cont==6){
+	    		timer.stop();
+			}
+		}
 	}
-
+    
+    
 	class PointSelector extends MouseAdapter{
 		PointSelector(Grafico grafico) {
 			g = grafico;
 			g.addMouseListener(this);
-			i = 1;
 		}
 
 		public void mousePressed(MouseEvent e) {
 			cont = 0;
-			cambiaImagen();
+			i=1;
+			timer.start();
 		}
 	}
 }
